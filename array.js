264ec8dd427a84de5a56a9831589e8b6e6daefd4
Senthil-Engr <hacksenlee@gmail.com>
2024-08-27 22:29:35 +0530

    slice example

--- array.js
+++ array.js
@@ -1,6 +1,14 @@
+/* push and pop
 var a = [1,2,3]
 a.push(4);
 console.log(a);
-a.pop(2);
+a.pop();
 console.log(a);
 
+*/
+
+// slice and splice
+var a = [1,2,3,4,5,6]
+var b = a.slice(0,3);
+console.log(b)
+console.log(a)
