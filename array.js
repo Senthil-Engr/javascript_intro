52c2b5bc7ee71f412dc42eb0ae56dba95fc8c5b5
Senthil-Engr <hacksenlee@gmail.com>
2024-09-05 21:38:51 +0530

    odd or even

--- array.js
+++ array.js
@@ -7,8 +7,14 @@ console.log(a);
 
 */
 
-// slice and splice
+// slice and 
+/*
 var a = [1,2,3,4,5,6]
 var b = a.slice(0,3);
 console.log(b)
 console.log(a)
+*/
+
+// splice
+var a = [1,2,3,4,5,6]
+var b = a.splice(2,1)
