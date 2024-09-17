fcb7e6d57b5d74f9e7aee31a148988114859f699
senthil-engr <hacksenlee@gmail.com>
2024-09-17 20:25:04 +0530

    dom file created

---
+++ arrayobject.js
@@ -0,0 +1,22 @@
+var engineer=[
+    {
+    name : "senthil",
+    emp_id : 11234,
+    Title : "Associate Engineer"
+    },
+    {
+        name : "karthick",
+        emp_id : 112342,
+        Title : "Engineer"
+    },
+    {
+            name : "kishore",
+            emp_id : 11784,
+            Title : "Associate Engineer"
+    }
+]
+
+
+for(count=0;count<3;count++){
+    console.log(engineer[count].emp_id);
+}
